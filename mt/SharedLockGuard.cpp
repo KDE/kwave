0250892552f344f5bcc3fe17ba641536de133863
Thomas Eschenbacher <Thomas.Eschenbacher@gmx.de>
2001-06-14 13:46:00 +0000

    initial checkin
    svn-id: https://kwave.svn.sourceforge.net/svnroot/kwave/trunk@158 bbf32e5a-961d-0410-9e10-91f32741c31e

---
+++ mt/SharedLockGuard.cpp
@@ -0,0 +1,35 @@
+/***************************************************************************
+    SharedLockGuard.cpp  -  Simple guard class for shared/exclusive locks
+			     -------------------
+    begin                : Jun 14 2001
+    copyright            : (C) 2001 by Thomas Eschenbacher
+    email                : Thomas Eschenbacher <thomas.eschenbacher@gmx.de>
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "mt/SharedLock.h"
+#include "mt/SharedLockGuard.h"
+
+//***************************************************************************
+SharedLockGuard::SharedLockGuard(SharedLock &lock, bool exclusive)
+    :TSS_Object(), m_lock(lock), m_exclusive(exclusive)
+{
+    m_lock.lock(m_exclusive);
+}
+
+//***************************************************************************
+SharedLockGuard::~SharedLockGuard()
+{
+    m_lock.unlock(m_exclusive);
+}
+
+//***************************************************************************
+//***************************************************************************

4f23013b1b82e18b1a0df6406ad0e21591d87643
Thomas Eschenbacher <Thomas.Eschenbacher@gmx.de>
2001-06-17 19:33:16 +0000

    initial checkin
    svn-id: https://kwave.svn.sourceforge.net/svnroot/kwave/trunk@161 bbf32e5a-961d-0410-9e10-91f32741c31e

---
+++ kwave/UndoDeleteAction.h
@@ -0,0 +1,53 @@
+/***************************************************************************
+     UndoDeleteAction.h  -  UndoAction for deletion of a range of samples
+			     -------------------
+    begin                : Jun 08 2001
+    copyright            : (C) 2001 by Thomas Eschenbacher
+    email                : Thomas Eschenbacher <thomas.eschenbacher@gmx.de>
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef _UNDO_DELETE_ACTION_H_
+#define _UNDO_DELETE_ACTION_H_
+
+#include <qstring.h>
+#include "UndoModifyAction.h"
+
+class SignalManager;
+
+class UndoDeleteAction: public UndoModifyAction
+{
+public:
+
+    /**
+     * Constructor.
+     * @param track index of the track
+     * @param offset index of the first sample to be deleted
+     * @param length number of samples to delete
+     */
+    UndoDeleteAction(unsigned int track, unsigned int offset,
+                     unsigned int length);
+
+    /** @see UndoAction::description() */
+    virtual QString description();
+
+    /** @see UndoAction::redoSize() */
+    virtual int redoSize();
+
+    /**
+     * Copies the samples to be deleted to the internal buffer.
+     * @see UndoAction::undo()
+     */
+    virtual UndoAction *undo(SignalManager &manager, bool with_redo);
+
+};
+
+#endif /* _UNDO_DELETE_ACTION_H_ */

2998708293ef08aa72f874979fb42c78235cc196
Thomas Eschenbacher <Thomas.Eschenbacher@gmx.de>
2001-11-02 20:08:33 +0000

    added experimental aRts interface stuff
    svn-id: https://kwave.svn.sourceforge.net/svnroot/kwave/trunk@272 bbf32e5a-961d-0410-9e10-91f32741c31e

--- plugins/amplifyfree/AmplifyFreePlugin.cpp
+++ plugins/amplifyfree/AmplifyFreePlugin.cpp
@@ -16,7 +16,9 @@
  ***************************************************************************/
 
 #include "config.h"
-#include "errno.h"
+#include <errno.h>
+#include <string.h> // ###
+#include <stdio.h> // ###
 
 #include <qarray.h> // ###
 #include <qstringlist.h>
@@ -24,6 +26,8 @@
 
 #include <arts/artsflow.h>
 #include <arts/connect.h>
+#include <arts/objectmanager.h> // ###
+#include <arts/stdsynthmodule.h> // ###
 
 #include "libkwave/InsertMode.h"
 #include "libkwave/MultiTrackWriter.h"
@@ -89,67 +93,112 @@ QStringList *AmplifyFreePlugin::setup(QStringList &previous_params)
     return list;
 };
 
+// example_add_impl.cc
+
+using namespace Arts;
+
+#include "kwave_sample_source.h"
+#include <stdsynthmodule.h>
+#include "libkwave/Sample.h" // ###
+#include "libkwave/SampleReader.h" // ###
+
+class Kwave_SampleSource_impl
+              :public Kwave_SampleSource_skel, Arts::StdSynthModule
+{
+public:
+
+     Kwave_SampleSource_impl()
+	:Kwave_SampleSource_skel(), Arts::StdSynthModule(),
+	source(0)
+     { }
+
+     Kwave_SampleSource_impl(SampleReader *src)
+	:Kwave_SampleSource_skel(), Arts::StdSynthModule(),
+	source(src)
+     { }
+
+     void calculateBlock(unsigned long samples)
+     {
+         unsigned long i;
+         for(i=0;i < samples;i++)
+             result[i] = (int)(double(i % 1024) * (double)((1 << 22) / 1024));
+     }
+
+protected:
+    SampleReader *source;
+
+};
+
+REGISTER_IMPLEMENTATION(Kwave_SampleSource_impl);
+
 //***************************************************************************
 void AmplifyFreePlugin::run(QStringList)
 {
-    UndoTransactionGuard undo_guard(*this, i18n("amplify free"));
+//    UndoTransactionGuard undo_guard(*this, i18n("amplify free"));
     m_stop = false;
-    QArray<sample_t> m_zeroes;
-    unsigned int ZERO_COUNT = 65536;
-
-    MultiTrackWriter writers;
-    manager().openMultiTrackWriter(writers, Overwrite);
-
-    // break if aborted
-    if (writers.isEmpty()) return;
-
-    unsigned int first = writers[0]->first();
-    unsigned int last  = writers[0]->last();
-    unsigned int count = writers.count();
-
-    // get the buffer with zeroes for faster filling
-    if (m_zeroes.count() != ZERO_COUNT) {
-	m_zeroes.resize(ZERO_COUNT);
-	m_zeroes.fill(0);
-    }
-
-    // loop over the sample range
-    while ((first <= last) && (!m_stop)) {
-	unsigned int rest = last - first + 1;
-	if (rest < m_zeroes.count()) m_zeroes.resize(rest);
-	
-	// loop over all writers
-	unsigned int w;
-	for (w=0; w < count; w++) {
-	    *(writers[w]) << m_zeroes;
-	}
-	
-	first += m_zeroes.count();
-    }
+//    QArray<sample_t> m_zeroes;
+//    unsigned int ZERO_COUNT = 65536;
+//
+//    MultiTrackWriter writers;
+//    manager().openMultiTrackWriter(writers, Overwrite);
+//
+//    // break if aborted
+//    if (writers.isEmpty()) return;
+//
+//    unsigned int first = writers[0]->first();
+//    unsigned int last  = writers[0]->last();
+//    unsigned int count = writers.count();
+//
+//    // get the buffer with zeroes for faster filling
+//    if (m_zeroes.count() != ZERO_COUNT) {
+//	m_zeroes.resize(ZERO_COUNT);
+//	m_zeroes.fill(0);
+//    }
+//
+//    // loop over the sample range
+//    while ((first <= last) && (!m_stop)) {
+//	unsigned int rest = last - first + 1;
+//	if (rest < m_zeroes.count()) m_zeroes.resize(rest);
+//	
+//	// loop over all writers
+//	unsigned int w;
+//	for (w=0; w < count; w++) {
+//	    *(writers[w]) << m_zeroes;
+//	}
+//	
+//	first += m_zeroes.count();
+//    }
 
     Arts::Dispatcher dispatcher;
+    dispatcher.lock();
 
-//    Synth_FREQUENCY freq1,freq2;   // object creation
-//    Synth_WAVE_SIN  sin1,sin2;
-//    Synth_PLAY      play;
-//
+    Kwave_SampleSource adapter = Kwave_SampleSource::_from_base(
+	new Kwave_SampleSource_impl((SampleReader*)0));
+
+    fprintf(stderr, "---%s:%d---\n",__FILE__, __LINE__);
+
+    Arts::Synth_PLAY      play;
+
 //    setValue(freq1, 440.0);	  // set frequencies
-//    setValue(freq2, 880.0);
-//
-//    connect(freq1, sin1);	  // object connection
-//    connect(freq2, sin2);
-//    connect(sin1, play, "invalue_left");
-//    connect(sin2, play, "invalue_right");
-//
-//    freq1.start(); freq2.start();  // start&go
-//    sin1.start(); sin2.start();
-//    play.start();
-//    dispatcher.run();
+//    Arts::connect(freq1, sin1);	  // object connection
+//    Arts::connect(sin1, play, "invalue_left");
+//    Arts::connect(sin2, play, "invalue_right");
 
+    fprintf(stderr, "---%s:%d\n",__FILE__, __LINE__);
+    Arts::connect(adapter, play, "invalue_left");
+    fprintf(stderr, "---%s:%d\n",__FILE__, __LINE__);
 
+    adapter.start();
+    play.start();
+    dispatcher.run();
 
-    writers.setAutoDelete(true);
-    writers.clear();
+    fprintf(stderr, "---%s:%d\n",__FILE__, __LINE__);
+
+//    writers.setAutoDelete(true);
+//    writers.clear();
+
+	fprintf(stderr, "---%s:%d---\n",__FILE__, __LINE__);
+	dispatcher.unlock();
 
     close();
 }

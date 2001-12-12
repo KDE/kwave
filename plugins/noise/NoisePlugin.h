8593ff8e7a0044f9307351e43b6c759ba40d9042
Thomas Eschenbacher <Thomas.Eschenbacher@gmx.de>
2001-12-12 18:08:08 +0000

    initial checkin
    svn-id: https://kwave.svn.sourceforge.net/svnroot/kwave/trunk@315 bbf32e5a-961d-0410-9e10-91f32741c31e

---
+++ plugins/noise/NoisePlugin.h
@@ -0,0 +1,46 @@
+/*************************************************************************
+    NoisePlugin.h  -  overwrites the selected range of samples with noise
+                             -------------------
+    begin                : Wed Dec 12 2001
+    copyright            : (C) 2001 by Thomas Eschenbacher
+    email                : Thomas.Eschenbacher@gmx.de
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef _NOISE_PLUGIN_H_
+#define _NOISE_PLUGIN_H_
+
+#include <qstring.h>
+#include <qstringlist.h>
+
+#include "libgui/KwavePlugin.h"
+
+class NoisePlugin: public KwavePlugin
+{
+    Q_OBJECT
+public:
+
+    /** Constructor */
+    NoisePlugin(PluginContext &c);
+
+    /** Fills the selected area with noise */
+    virtual void run(QStringList);
+
+    /** Aborts the process (if running). */
+    virtual int stop();
+
+private:
+    /** flag for stopping the process */
+    bool m_stop;
+};
+
+#endif /* _NOISE_PLUGIN_H_ */
+

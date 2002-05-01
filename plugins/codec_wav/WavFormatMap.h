dc9aa68c67e98bbea56393a0efa0c08ba15da8e8
Thomas Eschenbacher <Thomas.Eschenbacher@gmx.de>
2002-05-01 18:50:30 +0000

    initial checkin
    svn-id: https://kwave.svn.sourceforge.net/svnroot/kwave/trunk@504 bbf32e5a-961d-0410-9e10-91f32741c31e

---
+++ plugins/codec_wav/WavFormatMap.h
@@ -0,0 +1,40 @@
+/***************************************************************************
+         WavFormatMap.h  -  list of known wav file formats
+			     -------------------
+    begin                : Apr 28 2002
+    copyright            : (C) 2002 by Thomas Eschenbacher
+    email                : Thomas Eschenbacher <thomas.eschenbacher@gmx.de>
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef _WAV_FORMAT_MAP_H_
+#define _WAV_FORMAT_MAP_H_
+
+#include "config.h"
+#include <qmap.h>
+#include <qstring.h>
+#include "WavFileFormat.h"
+
+class QString;
+enum wav_format_id;
+
+class WavFormatMap: public QMap<wav_format_id, QString>
+{
+public:
+    /** Constructor, fills the map with all known types */
+    WavFormatMap();
+
+    /** Returns the name of an id, or "unknown" if not found. */
+    const QString &findName(unsigned int id);
+
+};
+
+#endif /* _WAV_FORMAT_MAP_H_ */

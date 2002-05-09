df6c475436767eee35e5bb4dc38cadd8f5be56b8
Thomas Eschenbacher <Thomas.Eschenbacher@gmx.de>
2002-05-09 14:51:20 +0000

    initial checkin/copy
    svn-id: https://kwave.svn.sourceforge.net/svnroot/kwave/trunk@516 bbf32e5a-961d-0410-9e10-91f32741c31e

---
+++ libaudiofile/modules/rebuffer.h
@@ -0,0 +1,10 @@
+#ifndef REBUFFER_H
+#define REBUFFER_H
+
+_AFmoduleinst _AFinitfloatrebufferv2f (AFframecount nsamps, bool multiple_of);
+_AFmoduleinst _AFinitfloatrebufferf2v (AFframecount nsamps, bool multiple_of);
+
+_AFmoduleinst _AFinitint2rebufferv2f (AFframecount nsamps, bool multiple_of);
+_AFmoduleinst _AFinitint2rebufferf2v (AFframecount nsamps, bool multiple_of);
+
+#endif /* REBUFFER_H */

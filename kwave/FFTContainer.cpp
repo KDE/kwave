621f301032adcdb9922a5490d024f12efad78cb4
Thomas Eschenbacher <Thomas.Eschenbacher@gmx.de>
2002-06-30 13:56:57 +0000

    got rid of the CornerPatchWidget
    svn-id: https://kwave.svn.sourceforge.net/svnroot/kwave/trunk@645 bbf32e5a-961d-0410-9e10-91f32741c31e

--- kwave/FFTContainer.cpp
+++ kwave/FFTContainer.cpp
@@ -1,54 +1,54 @@
-
-#include "config.h"
-#include <qpainter.h>
-#include <qpushbt.h>
-
-#include "libgui/FFTWidget.h"
-#include "libgui/ScaleWidget.h"
-#include "libgui/CornerPatchWidget.h"
-
-#include "FFTContainer.h"
-
-//****************************************************************************
-FFTContainer::FFTContainer (QWidget *parent):
-QWidget (parent)
-{
-    this->corner = 0;
-    this->view = 0;
-    this->xscale = 0;
-    this->yscale = 0;
-}
-
-//****************************************************************************
-void FFTContainer::setObjects(FFTWidget *view, ScaleWidget *x,
-                              ScaleWidget *y, CornerPatchWidget *corner)
-{
-    this->corner = corner;
-    this->view = view;
-    this->xscale = x;
-    this->yscale = y;
-}
-
-//****************************************************************************
-FFTContainer::~FFTContainer()
-{
-}
-
-//****************************************************************************
-void FFTContainer::resizeEvent(QResizeEvent *)
-{
-    ASSERT(corner);
-    ASSERT(view);
-    ASSERT(xscale);
-    ASSERT(yscale);
-    if (!corner) return;
-    if (!view) return;
-    if (!xscale) return;
-    if (!yscale) return;
-
-    int bsize = (QPushButton("test", this).sizeHint()).height();
-    view->setGeometry (bsize, 0, width() - bsize, height() - bsize);
-    xscale->setGeometry (bsize, height() - bsize, width() - bsize, bsize);
-    yscale->setGeometry (0, 0, bsize, height() - bsize);
-    corner->setGeometry (0, height() - bsize, bsize, bsize);
-}
+//
+//#include "config.h"
+//#include <qpainter.h>
+//#include <qpushbt.h>
+//
+//#include "libgui/FFTWidget.h"
+//#include "libgui/ScaleWidget.h"
+//// #include "libgui/CornerPatchWidget.h"
+//
+//#include "FFTContainer.h"
+//
+////****************************************************************************
+//FFTContainer::FFTContainer (QWidget *parent):
+//QWidget (parent)
+//{
+//    this->corner = 0;
+//    this->view = 0;
+//    this->xscale = 0;
+//    this->yscale = 0;
+//}
+//
+////****************************************************************************
+//void FFTContainer::setObjects(FFTWidget *view, ScaleWidget *x,
+//                              ScaleWidget *y, CornerPatchWidget *corner)
+//{
+//    this->corner = corner;
+//    this->view = view;
+//    this->xscale = x;
+//    this->yscale = y;
+//}
+//
+////****************************************************************************
+//FFTContainer::~FFTContainer()
+//{
+//}
+//
+////****************************************************************************
+//void FFTContainer::resizeEvent(QResizeEvent *)
+//{
+//    ASSERT(corner);
+//    ASSERT(view);
+//    ASSERT(xscale);
+//    ASSERT(yscale);
+//    if (!corner) return;
+//    if (!view) return;
+//    if (!xscale) return;
+//    if (!yscale) return;
+//
+//    int bsize = (QPushButton("test", this).sizeHint()).height();
+//    view->setGeometry (bsize, 0, width() - bsize, height() - bsize);
+//    xscale->setGeometry (bsize, height() - bsize, width() - bsize, bsize);
+//    yscale->setGeometry (0, 0, bsize, height() - bsize);
+//    corner->setGeometry (0, height() - bsize, bsize, bsize);
+//}

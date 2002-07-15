31baaf1c5ca1188628036f078cf38dab24b0a227
Thomas Eschenbacher <Thomas.Eschenbacher@gmx.de>
2002-07-15 17:19:19 +0000

    initial checkin
    svn-id: https://kwave.svn.sourceforge.net/svnroot/kwave/trunk@674 bbf32e5a-961d-0410-9e10-91f32741c31e

---
+++ plugins/codec_wav/WavPropertyMap.cpp
@@ -0,0 +1,43 @@
+/*************************************************************************
+     WavPropertyMap.cpp  -  map for translating properties to chunk names
+                             -------------------
+    begin                : Sat Jul 06 2002
+    copyright            : (C) 2002 by Thomas Eschenbacher
+    email                : Thomas.Eschenbacher@gmx.de
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "WavPropertyMap.h"
+
+//***************************************************************************
+WavPropertyMap::WavPropertyMap()
+{
+    insert("IARL", INF_ARCHIVAL      ); // archival location
+    insert("IART", INF_ARTIST	     ); // artist
+    insert("ICMS", INF_COMMISSIONED  ); // commissioned
+    insert("ICMT", INF_COMMENTS	     ); // comments
+    insert("ICOP", INF_COPYRIGHT     ); // copyright
+    insert("ICRD", INF_CREATION_DATE ); // creation date (iso)
+    insert("IENG", INF_ENGINEER	     ); // engineer
+    insert("IGNR", INF_GENRE	     ); // genre
+    insert("IKEY", INF_KEYWORDS	     ); // keywords
+    insert("IMED", INF_MEDIUM	     ); // medium
+    insert("INAM", INF_NAME	     ); // name
+    insert("IPRD", INF_PRODUCT	     ); // product
+    insert("ISFT", INF_SOFTWARE	     ); // software
+    insert("ISRC", INF_SOURCE	     ); // source
+    insert("ISRF", INF_SOURCE_FORM   ); // source form
+    insert("ITCH", INF_TECHNICAN     ); // technican
+    insert("ISBJ", INF_SUBJECT	     ); // subject
+}
+
+//***************************************************************************
+//***************************************************************************

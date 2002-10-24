0f8e8fb1e32de42998044b37bc5ee6a9775c72ed
Thomas Eschenbacher <Thomas.Eschenbacher@gmx.de>
2002-10-24 21:12:36 +0000

    initial checkin
    svn-id: https://kwave.svn.sourceforge.net/svnroot/kwave/trunk@787 bbf32e5a-961d-0410-9e10-91f32741c31e

---
+++ plugins/fileinfo/BitrateSpinBox.cpp
@@ -0,0 +1,113 @@
+/***************************************************************************
+     BitrateSpinBox.cpp  -  spinbox for selecting a bitrate for MP3 or Ogg
+			     -------------------
+    begin                : Thu Oct 24 2002
+    copyright            : (C) 2002 by Thomas Eschenbacher
+    email                : Thomas Eschenbacher <thomas.eschenbacher@gmx.de>
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "config.h"
+#include <stdlib.h> // for abs()
+
+#include "BitrateSpinBox.h"
+
+/***************************************************************************/
+BitrateSpinBox::BitrateSpinBox(QWidget *parent, const char *name)
+    :QSpinBox(parent, name), m_rates()
+{
+    m_rates.append(0); // don't let it stay empty, that makes life easier
+
+    connect(this, SIGNAL(valueChanged(int)),
+            this, SLOT(snapIn(int)));
+}
+
+/***************************************************************************/
+BitrateSpinBox::~BitrateSpinBox()
+{
+}
+
+/***************************************************************************/
+void BitrateSpinBox::snapIn(int value)
+{
+    int old_value = value;
+    int index = nearestIndex(old_value);
+    int new_value = m_rates[index];
+    QSpinBox::setValue(new_value);
+
+    if (new_value != old_value) {
+	emit snappedIn(new_value);
+    }
+}
+
+/***************************************************************************/
+void BitrateSpinBox::stepUp()
+{
+    int index = nearestIndex(value());
+    if (index < (int)m_rates.size()-1) {
+	index++;
+	int value = m_rates[index];
+	setValue(value);
+	emit snappedIn(value);
+    }
+}
+
+/***************************************************************************/
+void BitrateSpinBox::stepDown()
+{
+    int index = nearestIndex(value());
+    if (index > 0) {
+	index--;
+	int value = m_rates[index];
+	setValue(value);
+	emit snappedIn(value);
+    }
+}
+
+/***************************************************************************/
+void BitrateSpinBox::allowRates(const QValueList<int> &list)
+{
+    int old_value = value();
+
+    m_rates.clear();
+    m_rates += list;
+    if (m_rates.isEmpty()) m_rates.append(0);
+
+    // set new ranges
+    setMinValue(m_rates.first());
+    setMaxValue(m_rates.last());
+
+    setValue(old_value);
+}
+
+//***************************************************************************
+int BitrateSpinBox::nearestIndex(int rate)
+{
+    // find the nearest value
+    int nearest = 0;
+    QValueList<int>::iterator it;
+    for (it=m_rates.begin(); it != m_rates.end(); ++it)
+	if (abs(*it - rate) < abs(nearest - rate)) nearest = *it;
+
+    // find the index
+    int index = m_rates.findIndex(nearest);
+
+    // limit the index into a reasonable range
+    ASSERT(index >= 0);
+    ASSERT(index < (int)m_rates.size());
+    if (index < 0)                    index = 0;
+    if (index >= (int)m_rates.size()) index = m_rates.size()-1;
+
+    return index;
+}
+
+/***************************************************************************/
+/***************************************************************************/
---
+++ plugins/fileinfo/BitrateWidget.cpp
@@ -0,0 +1,118 @@
+/***************************************************************************
+      BitrateWidget.cpp  -  widget selecting a bitrate for MP3 or Ogg/Vorbis
+			     -------------------
+    begin                : Thu Oct 24 2002
+    copyright            : (C) 2002 by Thomas Eschenbacher
+    email                : Thomas Eschenbacher <thomas.eschenbacher@gmx.de>
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "config.h"
+#include <stdlib.h> // for abs()
+
+#include <qslider.h>
+#include <qspinbox.h>
+#include "BitrateSpinBox.h"
+#include "BitrateWidget.h"
+
+/***************************************************************************/
+BitrateWidget::BitrateWidget(QWidget *parent, const char *name)
+    :BitrateWidgetBase(parent, name), m_rates()
+{
+    m_rates.append(0); // don't let it stay empty, that makes life easier
+
+    connect(slider, SIGNAL(valueChanged(int)),
+            this, SLOT(sliderChanged(int)));
+    connect(slider, SIGNAL(sliderReleased()),
+            this, SLOT(snapInSlider()));
+    connect(spinbox, SIGNAL(snappedIn(int)),
+            slider, SLOT(setValue(int)));
+}
+
+/***************************************************************************/
+BitrateWidget::~BitrateWidget()
+{
+}
+
+/***************************************************************************/
+void BitrateWidget::setValue(int bitrate)
+{
+    slider->setValue(bitrate);
+    spinbox->setValue(bitrate);
+}
+
+/***************************************************************************/
+int BitrateWidget::value()
+{
+    int value = slider->value();
+    int index = nearestIndex(value);
+    return m_rates[index];
+}
+
+/***************************************************************************/
+void BitrateWidget::setSpecialValueText(const QString &text)
+{
+    spinbox->setSpecialValueText(text);
+}
+
+/***************************************************************************/
+void BitrateWidget::allowRates(const QValueList<int> &list)
+{
+    int old_value = value();
+    
+    m_rates.clear();
+    m_rates += list;
+    if (m_rates.isEmpty()) m_rates.append(0);
+
+    // set new ranges
+    spinbox->allowRates(m_rates);
+    slider->setMinValue(m_rates.first());
+    slider->setMaxValue(m_rates.last());
+
+    setValue(old_value);
+}
+
+//***************************************************************************
+int BitrateWidget::nearestIndex(int rate)
+{
+    // find the nearest value
+    int nearest = 0;
+    QValueList<int>::iterator it;
+    for (it=m_rates.begin(); it != m_rates.end(); ++it)
+	if (abs(*it - rate) < abs(nearest - rate)) nearest = *it;
+
+    // find the index
+    int index = m_rates.findIndex(nearest);
+
+    // limit the index into a reasonable range
+    if (index < 0)                    index = 0;
+    if (index >= (int)m_rates.size()) index = m_rates.size()-1;
+
+    return index;
+}
+
+//***************************************************************************
+void BitrateWidget::sliderChanged(int value)
+{
+    int index = nearestIndex(value);
+    spinbox->setValue(m_rates[index]);
+}
+
+//***************************************************************************
+void BitrateWidget::snapInSlider()
+{
+    int value = slider->value();
+    int index = nearestIndex(value);
+    slider->setValue(m_rates[index]); // snap in
+}
+
+/***************************************************************************/
+/***************************************************************************/

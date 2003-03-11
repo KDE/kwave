7928cdb71fc48e2a39ff8c546146dfd9b1735525
Thomas Eschenbacher <Thomas.Eschenbacher@gmx.de>
2003-03-11 18:47:41 +0000

    implemented logarithmic scale, some minor fixes
    svn-id: https://kwave.svn.sourceforge.net/svnroot/kwave/trunk@939 bbf32e5a-961d-0410-9e10-91f32741c31e

--- libgui/ScaleWidget.cpp
+++ libgui/ScaleWidget.cpp
@@ -16,7 +16,10 @@
  ***************************************************************************/
 
 #include "config.h"
+#include <stdlib.h>
+#include <math.h>
 #include <stdio.h>
+
 #include <qpixmap.h>
 #include <qimage.h>
 #include <qdir.h>
@@ -114,6 +117,10 @@ void ScaleWidget::paintText(QPainter &p, int x, int y, int ofs,
 //***************************************************************************
 void ScaleWidget::drawLog(QPainter &p, int w, int h, bool inverse)
 {
+    // only use base 10 for now, tested with others too,
+    // but not configurable through a property
+    const int base = 10;
+    
     int dir = (inverse) ? -1 : +1;
 
     p.setPen (colorGroup().dark());
@@ -121,6 +128,46 @@ void ScaleWidget::drawLog(QPainter &p, int w, int h, bool inverse)
     p.drawLine (dir*(w-1), 0, dir*(w-1), dir*(h-1));
 
     p.setPen (colorGroup().text());
+
+    int a, x;
+    double ofs;
+    const int h2 = h;
+
+    ASSERT(m_low >= 0);
+    ASSERT(m_high > m_low);
+    
+    int dec_lo = (m_low) ? (int)floor(log(m_low)/log(base)) : 0;
+    int dec_hi = (int)ceil(log(m_high)/log(base));
+    int decades = abs(dec_hi - dec_lo) + 1;
+
+    // check if we have enough space for the small lines within a decade
+    int w1 = (int)(w / decades); // pixels per decade
+    bool small_lines = (w1 - (int)((double)w1 * log(base-1)/log(base))) > 1;
+    
+    // print the lines
+    for (a = 0; a < decades; a++) {
+	// big line, for each decade
+	x = (int)((w-1) * a / decades);
+	p.drawLine (dir*x, dir*1, dir*x, dir*(h2-2));
+
+	w1 = (int)((w-1) * (a+1) / decades) - x + 1;
+	if (small_lines) {
+	    // small lines, within the decade
+	    for (int i=1; i < base; i++) {
+		int x1 = x + (int)((double)w1 * log(i)/log(base));
+		p.drawLine (dir*x1, dir*1, dir*x1, dir*((h2/2)-2));
+	    }
+	}
+    }
+
+    // print the text
+    for (a = 0; a < decades; a++) {
+	QString buf = "%1 %2";
+	int value = (int)pow(base, dec_lo+a);
+	buf = buf.arg(value).arg(m_unittext);
+	x = ((w-1) * a)/decades;
+	paintText(p, dir*(x+2), dir*(h-FONTSIZE-2), FONTSIZE, inverse, buf);
+    }
 }
 
 //***************************************************************************
@@ -142,21 +189,21 @@ void ScaleWidget::drawLinear(QPainter &p, int w, int h, bool inverse)
     // print the lines
     while ((t / 10 > 1) && (h2 > 0)) {
 	for (ofs = 0; ofs < w - 1; ofs += t) {
-	    for (a = 0; a < 6; a++) {
-		x = (int)(ofs + (t * a / 5));
+	    for (a = 0; a < 4; a++) {
+		x = (int)(ofs + (t * a / 4));
 		p.drawLine (dir*x, dir*1, dir*x, dir*(h2-2));
 	    }
 	}
 	h2 >>= 1;
-	t /= 5;
+	t /= 4;
     }
 
     // print the text
-    for (a = 0; a < 5; a++) {
-	char buf[64];
-	snprintf(buf, sizeof(buf), "%d %s", m_low + (((m_high - m_low)*a)/5),
-	         m_unittext.data());
-	x = ((w-1) * a)/5;
+    for (a = 0; a < 4; a++) {
+	QString buf = "%1 %2";
+	int value = m_low + (((m_high - m_low)* (inverse ? (4-a) : a))/4);
+	buf = buf.arg(value).arg(m_unittext);
+	x = ((w-1) * a)/4;
 	paintText(p, dir*(x+2), dir*(h-FONTSIZE-2), FONTSIZE, inverse, buf);
     }
 

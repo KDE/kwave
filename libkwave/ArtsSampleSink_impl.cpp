936ef0423d02329e9383039018eb6f0ad956a855
Thomas Eschenbacher <Thomas.Eschenbacher@gmx.de>
2003-05-31 20:12:07 +0000

    use float2sample instead of manual calculation
    svn-id: https://kwave.svn.sourceforge.net/svnroot/kwave/trunk@971 bbf32e5a-961d-0410-9e10-91f32741c31e

--- libkwave/ArtsSampleSink_impl.cpp
+++ libkwave/ArtsSampleSink_impl.cpp
@@ -44,12 +44,10 @@ void ArtsSampleSink_impl::calculateBlock(unsigned long samples)
     if (!m_writer) return;
 
     unsigned long i;
-    sample_t sample = 0;
 
     // fill the sink with our input
     for(i=0;i < samples;i++) {
-	sample = sample_t(sink[i] * (1 << 23));
-	*m_writer << sample;
+	*m_writer << float2sample(sink[i]);
     }
 }
 

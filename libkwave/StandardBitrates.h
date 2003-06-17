79463f1d66f60caebcd1ba78ef25577a49d36586
Thomas Eschenbacher <Thomas.Eschenbacher@gmx.de>
2003-06-17 19:48:24 +0000

    initial checkin
    svn-id: https://kwave.svn.sourceforge.net/svnroot/kwave/trunk@1009 bbf32e5a-961d-0410-9e10-91f32741c31e

---
+++ libkwave/StandardBitrates.h
@@ -0,0 +1,45 @@
+/*************************************************************************
+     StandardBitrates.h  -  well known bitrates from ogg/vorbis + MP3
+                             -------------------
+    begin                : Tue Jun 17 2003
+    copyright            : (C) 2003 by Thomas Eschenbacher
+    email                : Thomas.Eschenbacher@gmx.de
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef _STANDARD_BITRATES_H_
+#define _STANDARD_BITRATES_H_
+
+#include "config.h"
+#include <qvaluelist.h>
+
+class StandardBitrates: public QValueList<int>
+{
+public:
+    /** Constructor */
+    StandardBitrates();
+
+    /** Destructor */
+    virtual ~StandardBitrates();
+    
+    /** returns a const reference to the list of bitrates */
+    static const StandardBitrates &instance();
+
+    /**
+     * Returns the standard bitrate that is nearest to the given one
+     * @param rate given rate in [bits/second]
+     * @return nearest bitrate in [bits/second]
+     */
+    virtual int nearest(int rate) const;
+    
+};
+
+#endif /* _STANDARD_BITRATES_H_ */

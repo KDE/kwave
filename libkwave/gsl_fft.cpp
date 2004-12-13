96163e569a68d352585be4555d37c43f8909fb29
Thomas Eschenbacher <Thomas.Eschenbacher@gmx.de>
2004-12-13 19:40:15 +0000

    added missing copyright messages in the file headers
    svn-id: https://kwave.svn.sourceforge.net/svnroot/kwave/trunk@1333 bbf32e5a-961d-0410-9e10-91f32741c31e

--- libkwave/gsl_fft.cpp
+++ libkwave/gsl_fft.cpp
@@ -1,3 +1,22 @@
+/* fft/*.c
+ *
+ * Copyright (C) 1996, 1997, 1998, 1999, 2000 Brian Gough
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
 //code taken from gnu gsl project
 //this code is part of the gnu gsl library. The functions being useful
 //for kwave were concatenated, since gsl library is not far spread
@@ -94,7 +113,7 @@ gsl_fft_complex_init (unsigned int n,
 
     status = gsl_fft_complex_factorize (n, &n_factors, wavetable->factor);
     if (!status) return status;
-    
+
     wavetable->nf = n_factors;
 
     status = gsl_fft_complex_generate_wavetable (n, wavetable);
--- libkwave/gsl_fft.h
+++ libkwave/gsl_fft.h
@@ -1,3 +1,21 @@
+/* fft/gsl_fft.h
+ *
+ * Copyright (C) 1996, 1997, 1998, 1999, 2000 Brian Gough
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
 #ifndef _GSL_FFT_H
 #define _GSL_FFT_H
 
@@ -9,9 +27,9 @@ typedef enum
 gsl_fft_direction;
 
 /* this give the sign in the formula
- 
-   h(f) = \sum x(t) exp(+/- 2 pi i f t) 
-       
+
+   h(f) = \sum x(t) exp(+/- 2 pi i f t)
+
    where - is the forward transform direction and + the inverse direction */
 
 

e46294864f9395d710ca4ed958d15ceddc4254f4
Thomas Eschenbacher <Thomas.Eschenbacher@gmx.de>
2005-04-01 18:08:17 +0000

    simplified a lot, we can use the Qt-3 methods here...
    svn-id: https://kwave.svn.sourceforge.net/svnroot/kwave/trunk@1432 bbf32e5a-961d-0410-9e10-91f32741c31e

--- mt/ThreadsafeX11Guard.cpp
+++ mt/ThreadsafeX11Guard.cpp
@@ -15,137 +15,23 @@
  *                                                                         *
  ***************************************************************************/
 
-#include <qapplication.h> // for flushX and syncX
-#include <qmutex.h>
-
-#include "mt/MutexGuard.h"
-#include "mt/Semaphore.h"
+#include "config.h"
+#include <qapplication.h>
 #include "mt/ThreadsafeX11Guard.h"
 
 //***************************************************************************
-
-/* static initializer for the global/static X11 lock */
-QMutex ThreadsafeX11Guard::m_lock_X11;
-
-/* static initializer for the internal/static X11 lock */
-QMutex ThreadsafeX11Guard::m_internal_lock;
-
-/* static initializer for the lock for protection of recursion and owner */
-QMutex ThreadsafeX11Guard::m_lock_recursion;
-
-/* static initializer for the thread that currently has locked X11 */
-pthread_t ThreadsafeX11Guard::m_pid_owner = pthread_self();
-
-/* static initializer for the thread that owns X11 (main thread) */
-pthread_t ThreadsafeX11Guard::m_pid_x11 = pthread_self();
-
-/* static initializer for the counter of recursive X11 locks. */
-unsigned int ThreadsafeX11Guard::m_recursion_level = 0;
-
-//***************************************************************************
 ThreadsafeX11Guard::ThreadsafeX11Guard()
-    :QObject(), m_sem_x11_locked(), m_sem_x11_done(),
-     m_sem_x11_unlocked(), m_spx_X11_request(this, SLOT(lockX11()))
+    :QObject()
 {
-    if (m_pid_x11 == pthread_self()) {
-	return;
-    }
-
-    {
-	MutexGuard lock(m_lock_recursion);
-	if (m_pid_owner == pthread_self()) {
-	    // recursive enter
-	    m_recursion_level++;
-	    return;
-	}
-    }
-
-    // lock the X11 system, no others may use it now
-    m_internal_lock.lock();
-
-    // activate the X11 thread
-    m_spx_X11_request.AsyncHandler();
-
-    // sometimes the X11 / GUI thread needs an extra wakeup
     Q_ASSERT(qApp);
-    if (qApp) qApp->wakeUpGuiThread();
-
-    // wait until the X11 thread is locked and suspended
-    m_sem_x11_locked.wait();
-
-    // now we are the only owner of X11
-    {
-	MutexGuard lock(m_lock_recursion);
-	m_pid_owner = pthread_self();
-	m_recursion_level = 1;
-    }
+    if (qApp) qApp->lock();
 }
 
 //***************************************************************************
 ThreadsafeX11Guard::~ThreadsafeX11Guard()
 {
-    if (m_pid_x11 == pthread_self()) {
-	return;
-    }
-
-    {
-	MutexGuard lock(m_lock_recursion);
-
-	// decrease the recursion level (should always be at least 1)
-	Q_ASSERT(m_recursion_level);
-	if (m_recursion_level) m_recursion_level--;
-
-	// break if only recursion decreased and zero not reached
-	if (m_recursion_level) return;
-    }
-
-    // flush all X11 events
-    QApplication::flushX();
-
-    // let the X11 thread continue
-    m_sem_x11_done.post();
-
-    // wait until X11 is unlocked again
-    m_sem_x11_unlocked.wait();
-
-    // release the control over X11
-    {
-	MutexGuard lock(m_lock_recursion);
-	m_pid_owner = 0;
-	m_recursion_level = 0;
-    }
-
-    // sometimes the X11 / GUI thread needs an extra wakeup
     Q_ASSERT(qApp);
-    if (qApp) qApp->wakeUpGuiThread();
-
-    // release the X11 system and let others lock it
-    m_internal_lock.unlock();
-}
-
-//***************************************************************************
-void ThreadsafeX11Guard::lockX11()
-{
-    // lock the global/static X11 mutex
-    m_lock_X11.lock();
-
-    // be sure there are no other X11 transactions we interrupt
-    // so better give them a chance to complete within the
-    // context of our thread
-    QApplication::syncX();
-    QApplication::flushX();
-
-    // notify the waiting worker thread that X11 is locked now
-    m_sem_x11_locked.post();
-
-    // suspend until the worker thread is done with X11
-    m_sem_x11_done.wait();
-
-    // unlock X11 again
-    m_lock_X11.unlock();
-
-    // notify the worker thread that X11 is available/unlocked again
-    m_sem_x11_unlocked.post();
+    if (qApp) qApp->unlock();
 }
 
 //***************************************************************************
--- mt/ThreadsafeX11Guard.h
+++ mt/ThreadsafeX11Guard.h
@@ -22,9 +22,6 @@
 #include <qmutex.h>
 #include <qobject.h>
 
-#include "mt/SignalProxy.h"
-#include "mt/Semaphore.h"
-
 /**
  * @class ThreadsafeX11Guard
  * If a thread other than the program's main thread wants to access X11,
@@ -52,69 +49,6 @@ public:
     /** Destructor */
     virtual ~ThreadsafeX11Guard();
 
-private slots:
-
-    /**
-     * This slot is called in the context of the X11 thread and locks
-     * X11 while the worker thread is active. The handshake process
-     * with the worker thread is done via some semaphores.
-     * @see m_sem_x11_locked
-     * @see m_sem_x11_done
-     * @see m_sem_x11_unlocked
-     */
-    void lockX11();
-
-private:
-
-    /** The global/unique lock for X11 */
-    static QMutex m_lock_X11;
-
-    /**
-     * Internal lock for access to the X11 subsystem
-     */
-    static QMutex m_internal_lock;
-
-    /**
-     * The mutex for entering/leaving a protected area, used in our
-     * constructor and destructor. Protects m_recursion_level and
-     * m_pid_owner
-     */
-    static QMutex m_lock_recursion;
-
-    /** thread id of the thread that is currently holding the X11 lock */
-    static pthread_t m_pid_owner;
-
-    /** thread id of the X11 thread (main thread) */
-    static pthread_t m_pid_x11;
-
-    /** counter for recursive X11 locks. */
-    static unsigned int m_recursion_level;
-
-    /**
-     * Semaphore set by lockX11() to signal that X11 has been locked and
-     * the X11 thread will be suspended.
-     */
-    Semaphore m_sem_x11_locked;
-
-    /**
-     * Semaphore set by the worker thread to signal the X11 thread that
-     * is currently suspended in lockX11() that it can continue and
-     * release X11 again.
-     */
-    Semaphore m_sem_x11_done;
-
-    /**
-     * Semaphore set by lockX11() to signal that X11 has been unlocked
-     * again and the X11 thread is active again.
-     */
-    Semaphore m_sem_x11_unlocked;
-
-    /**
-     * The SignalProxy activates the X11 thread and lets it enter lockX11()
-     */
-    SignalProxy<void> m_spx_X11_request;
-
 };
 
 #endif /* _THREADSAFE_X11_GUARD_H_ */
-

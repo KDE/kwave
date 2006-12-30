4618d588d897d4196667358d5cbb736fb0fcb4e3
Thomas Eschenbacher <Thomas.Eschenbacher@gmx.de>
2006-12-30 06:39:47 +0000

    went back to old implementation of ThreadsafeX11Guard class in order to fix a deadlock (closes sourceforge bug #1623357)
    svn-id: https://kwave.svn.sourceforge.net/svnroot/kwave/trunk@1772 bbf32e5a-961d-0410-9e10-91f32741c31e

--- mt/ThreadsafeX11Guard.h
+++ mt/ThreadsafeX11Guard.h
@@ -20,7 +20,10 @@
 
 #include "config.h"
 #include <qmutex.h>
+#include <qsemaphore.h>
 #include <qobject.h>
+
+#include "mt/SignalProxy.h"
 
 /**
  * @class ThreadsafeX11Guard
@@ -48,6 +51,68 @@ public:
 
     /** Destructor */
     virtual ~ThreadsafeX11Guard();
+
+private slots:
+
+    /**
+     * This slot is called in the context of the X11 thread and locks
+     * X11 while the worker thread is active. The handshake process
+     * with the worker thread is done via some semaphores.
+     * @see m_sem_x11_locked
+     * @see m_sem_x11_done
+     * @see m_sem_x11_unlocked
+     */
+    void lockX11();
+
+private:
+
+    /** The global/unique lock for X11 */
+    static QMutex m_lock_X11;
+
+    /**
+     * Internal lock for access to the X11 subsystem
+     */
+    static QMutex m_internal_lock;
+
+    /**
+     * The mutex for entering/leaving a protected area, used in our
+     * constructor and destructor. Protects m_recursion_level and
+     * m_pid_owner
+     */
+    static QMutex m_lock_recursion;
+
+    /** thread id of the thread that is currently holding the X11 lock */
+    static pthread_t m_pid_owner;
+
+    /** thread id of the X11 thread (main thread) */
+    static pthread_t m_pid_x11;
+
+    /** counter for recursive X11 locks. */
+    static unsigned int m_recursion_level;
+
+    /**
+     * Semaphore set by lockX11() to signal that X11 has been locked and
+     * the X11 thread will be suspended.
+     */
+    QSemaphore m_sem_x11_locked;
+
+    /**
+     * Semaphore set by the worker thread to signal the X11 thread that
+     * is currently suspended in lockX11() that it can continue and
+     * release X11 again.
+     */
+    QSemaphore m_sem_x11_done;
+
+    /**
+     * Semaphore set by lockX11() to signal that X11 has been unlocked
+     * again and the X11 thread is active again.
+     */
+    QSemaphore m_sem_x11_unlocked;
+
+    /**
+     * The SignalProxy activates the X11 thread and lets it enter lockX11()
+     */
+    SignalProxy<void> m_spx_X11_request;
 
 };
 

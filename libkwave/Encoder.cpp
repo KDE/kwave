7af1c091cf6cbcbc0666dac508d225cbab17f37f
Thomas Eschenbacher <Thomas.Eschenbacher@gmx.de>
2007-05-11 15:24:39 +0000

    added cpp files for Encoder and Selection (needed for automoc)
    svn-id: https://kwave.svn.sourceforge.net/svnroot/kwave/trunk@1975 bbf32e5a-961d-0410-9e10-91f32741c31e

---
+++ libkwave/Encoder.cpp
@@ -0,0 +1,29 @@
+/***************************************************************************
+            Encoder.cpp  -  abstract base class of all encoders
+			     -------------------
+    begin                : May 08 2007
+    copyright            : (C) 20027 by Thomas Eschenbacher
+    email                : Thomas Eschenbacher <thomas.eschenbacher@gmx.de>
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include <Encoder.h>
+
+//***************************************************************************
+Encoder::Encoder()
+    :QObject(), CodecBase()
+{
+}
+
+//***************************************************************************
+#include "Encoder.moc"
+//***************************************************************************
+//***************************************************************************
---
+++ libkwave/Selection.cpp
@@ -0,0 +1,29 @@
+/***************************************************************************
+           Selection.cpp - Simple class for a selection
+			     -------------------
+    begin                : Tue May 08 2007
+    copyright            : (C) 2007 by Thomas Eschenbacher
+    email                : Thomas.Eschenbacher@gmx.de
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "Selection.h"
+
+//***************************************************************************
+Selection::Selection(unsigned int offset, unsigned int length)
+    :QObject(), m_offset(offset), m_length(length)
+{
+}
+
+//***************************************************************************
+#include "Selection.moc"
+//***************************************************************************
+//***************************************************************************

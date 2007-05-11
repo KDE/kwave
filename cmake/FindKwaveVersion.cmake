e50490b6631e39b62913bb210d5524dd36fe9c70
Thomas Eschenbacher <Thomas.Eschenbacher@gmx.de>
2007-05-11 19:03:26 +0000

    first version of cmake build system, core and libs only
    svn-id: https://kwave.svn.sourceforge.net/svnroot/kwave/trunk@1976 bbf32e5a-961d-0410-9e10-91f32741c31e

---
+++ cmake/FindKwaveVersion.cmake
@@ -0,0 +1,50 @@
+#############################################################################
+##    Kwave                - cmake/FindKwaveVersion.cmake
+##                           -------------------
+##    begin                : Fri May 11 2007
+##    copyright            : (C) 2007 by Thomas Eschenbacher
+##    email                : Thomas.Eschenbacher@gmx.de
+#############################################################################
+#
+#############################################################################
+##                                                                          #
+##    This program is free software; you can redistribute it and/or modify  #
+##    it under the terms of the GNU General Public License as published by  #
+##    the Free Software Foundation; either version 2 of the License, or     #
+##    (at your option) any later version.                                   #
+##                                                                          #
+#############################################################################
+
+#############################################################################
+### read the version info from the "VERSION" file in the toplevel dir     ###
+FILE(READ ${CMAKE_CURRENT_SOURCE_DIR}/VERSION KWAVE_VERSION_RAW)
+
+#############################################################################
+### split into major.minor.release                                        ###
+STRING(REGEX MATCHALL "([0-9]+)" KWAVE_VERSION_LIST ${KWAVE_VERSION_RAW})
+LIST(GET KWAVE_VERSION_LIST 0 KWAVE_VERSION_MAJOR)
+LIST(GET KWAVE_VERSION_LIST 1 KWAVE_VERSION_MINOR)
+LIST(GET KWAVE_VERSION_LIST 2 KWAVE_VERSION_RELEASE)
+
+#############################################################################
+### re-assemble full version number: major.minor.release                  ###
+SET(KWAVE_VERSION_FULL "${KWAVE_VERSION_MAJOR}.${KWAVE_VERSION_MINOR}.${KWAVE_VERSION_RELEASE}")
+
+#############################################################################
+### optional: a patchlevel separated with a "-"                           ###
+LIST(LENGTH KWAVE_VERSION_LIST COUNT)
+IF (${COUNT} GREATER 3)
+    LIST(GET KWAVE_VERSION_LIST 3 KWAVE_VERSION_PATCHLEVEL)
+    SET(KWAVE_VERSION_FULL "${KWAVE_VERSION_FULL}-${KWAVE_VERSION_PATCHLEVEL}")
+ENDIF (${COUNT} GREATER 3)
+
+#############################################################################
+### version number for .so files                                          ###
+SET(KWAVE_SOVERSION "${KWAVE_VERSION_MAJOR}.${KWAVE_VERSION_MINOR}.${KWAVE_VERSION_RELEASE}")
+
+#############################################################################
+### status output                                                         ###
+MESSAGE(STATUS "Building Kwave version ${KWAVE_VERSION_FULL}")
+
+#############################################################################
+#############################################################################

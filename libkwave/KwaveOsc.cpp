4eb1ae1fde2f482b7331734ac61027f8cd231b66
Thomas Eschenbacher <Thomas.Eschenbacher@gmx.de>
2007-11-12 17:49:49 +0000

    Kwave::Osc
    svn-id: https://kwave.svn.sourceforge.net/svnroot/kwave/trunk@2042 bbf32e5a-961d-0410-9e10-91f32741c31e

---
+++ libkwave/KwaveOsc.cpp
@@ -0,0 +1,80 @@
+/***************************************************************************
+           KwaveOsc.cpp  -  simple sine oscillator
+                             -------------------
+    begin                : Tue Nov 06 2007
+    copyright            : (C) 2007 by Thomas Eschenbacher
+    email                : Thomas.Eschenbacher@gmx.de
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "config.h"
+#include <math.h>
+#include "libkwave/KwaveOsc.h"
+
+//***************************************************************************
+Kwave::Osc::Osc()
+    :Kwave::SampleSource(0, "Kwave::Osc"),
+    m_buffer(blockSize()), m_omega_t(0.0), m_f(44.1), m_a(1.0)
+{
+}
+
+//***************************************************************************
+Kwave::Osc::~Osc()
+{
+}
+
+//***************************************************************************
+void Kwave::Osc::goOn()
+{
+    unsigned int samples = m_buffer.size();
+    const double two_pi = 2.0 * M_PI;
+
+    Q_ASSERT(m_f != 0);
+    if (m_f == 0) return;
+
+    double omega = two_pi / m_f;
+    for (unsigned int sample = 0; sample < samples; sample++) {
+	// calculate one sample as sin(w * t)
+	m_buffer[sample] = double2sample(m_a * sin(m_omega_t));
+
+	// next time, t++
+	m_omega_t += omega;
+
+	// limit the argument of sin() to [0 ... 4*Pi]
+	while (m_omega_t > two_pi)
+	    m_omega_t -= two_pi;
+    }
+
+    emit output(m_buffer);
+}
+
+//***************************************************************************
+void Kwave::Osc::setFrequency(const QVariant &f)
+{
+    m_f = QVariant(f).asDouble();
+}
+
+//***************************************************************************
+void Kwave::Osc::setPhase(const QVariant &p)
+{
+    m_omega_t = QVariant(p).asDouble();
+}
+
+//***************************************************************************
+void Kwave::Osc::setAmplitude(const QVariant &a)
+{
+    m_a = QVariant(a).asDouble();
+}
+
+//***************************************************************************
+#include "KwaveOsc.moc"
+//***************************************************************************
+//***************************************************************************
---
+++ libkwave/KwaveOsc.h
@@ -0,0 +1,83 @@
+/***************************************************************************
+             KwaveOsc.h  -  simple sine oscillator
+                             -------------------
+    begin                : Tue Nov 06 2007
+    copyright            : (C) 2007 by Thomas Eschenbacher
+    email                : Thomas.Eschenbacher@gmx.de
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef _KWAVE_OSC_H_
+#define _KWAVE_OSC_H_
+
+#include "config.h"
+#include <qobject.h>
+#include <qvariant.h>
+
+#include "libkwave/KwaveSampleSource.h"
+
+namespace Kwave {
+
+    class Osc: public Kwave::SampleSource
+    {
+	Q_OBJECT
+	public:
+	    /** Constructor */
+	    Osc();
+
+	    /** Destructor */
+	    virtual ~Osc();
+
+	    /** does the calculation */
+	    virtual void goOn();
+
+	signals:
+	    /** emits a block with sine wave data */
+	    void output(Kwave::SampleArray &data);
+
+	public slots:
+	    /**
+	     * Sets the frequency of the sine wave, normed to the
+	     * sample frequency. You should pass the frequency that
+	     * you want, divided through the sample frequency.
+	     * If you never call this, the frequency will be undefined!
+	     */
+	    void setFrequency(const QVariant &f);
+
+	    /**
+	     * Sets the phase of the sine wave in RAD [0...2*Pi].
+	     * The default setting is zero.
+	     */
+	    void setPhase(const QVariant &p);
+
+	    /**
+	     * Sets the amplitude of the sine wave, normed to the
+	     * range of [0.0 ... 1.0]. The default is 1.0.
+	     */
+	    void setAmplitude(const QVariant &a);
+
+	private:
+
+	    /** buffer for output data */
+	    Kwave::SampleArray m_buffer;
+
+	    /** current time multiplied by 2*Pi*f */
+	    double m_omega_t;
+
+	    /** frequency [samples/period] */
+	    double m_f;
+
+	    /** amplitude [0...1] */
+	    double m_a;
+    };
+}
+
+#endif /* _KWAVE_OSC_H_ */

6f93a53a879b9fa11c9816d9432175558dd662c4
Thomas Eschenbacher <Thomas.Eschenbacher@gmx.de>
2008-05-12 15:00:04 +0000

    implemented wrapper for QTreeView that emits a 'focusLost()' signal
    svn-id: https://kwave.svn.sourceforge.net/svnroot/kwave/branches/porting-kde4@2124 bbf32e5a-961d-0410-9e10-91f32741c31e

---
+++ libgui/TreeWidgetWrapper.cpp
@@ -0,0 +1,42 @@
+/***************************************************************************
+    TreeWidget.cpp  -  wrapper for QTreeWidget to get focus out information
+			     -------------------
+    begin                : Mon May 12 2008
+    copyright            : (C) 2008 by Thomas Eschenbacher
+    email                : Thomas.Eschenbacher@gmx.de
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "config.h"
+#include "TreeWidgetWrapper.h"
+
+//***************************************************************************
+TreeWidgetWrapper::TreeWidgetWrapper(QWidget *widget)
+    :QTreeWidget(widget)
+{
+}
+
+//***************************************************************************
+TreeWidgetWrapper::~TreeWidgetWrapper()
+{
+}
+
+//***************************************************************************
+void TreeWidgetWrapper::focusOutEvent(QFocusEvent *event)
+{
+    QTreeWidget::focusOutEvent(event);
+    emit focusLost();
+};
+
+//***************************************************************************
+#include "TreeWidgetWrapper.moc"
+//***************************************************************************
+//***************************************************************************
---
+++ libgui/TreeWidgetWrapper.h
@@ -0,0 +1,48 @@
+/***************************************************************************
+    TreeWidget.h  -  wrapper for QTreeWidget to get focus out information
+			     -------------------
+    begin                : Mon May 12 2008
+    copyright            : (C) 2008 by Thomas Eschenbacher
+    email                : Thomas.Eschenbacher@gmx.de
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef _TREE_WIDGET_WRAPPER_H_
+#define _TREE_WIDGET_WRAPPER_H_
+
+#include "config.h"
+
+#include <QObject>
+#include <QTreeWidget>
+
+class QFocusEvent;
+
+class TreeWidgetWrapper: public QTreeWidget
+{
+    Q_OBJECT
+public:
+    /** Constructor */
+    TreeWidgetWrapper(QWidget *parent);
+
+    /** Destructor */
+    virtual ~TreeWidgetWrapper();
+
+    /** catches the "lost focus" event */
+    virtual void focusOutEvent(QFocusEvent *event);
+
+signals:
+
+    /** emitted when the focus has been lost */
+    void focusLost();
+
+};
+
+#endif /* _TREE_WIDGET_WRAPPER_H_ */

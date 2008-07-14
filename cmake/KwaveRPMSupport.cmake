cf52f063c90a61da2601112df718a16cb4279799
Thomas Eschenbacher <Thomas.Eschenbacher@gmx.de>
2008-07-14 19:01:23 +0000

    using bzip2 for tarball
    svn-id: https://kwave.svn.sourceforge.net/svnroot/kwave/trunk@2159 bbf32e5a-961d-0410-9e10-91f32741c31e

--- cmake/KwaveRPMSupport.cmake
+++ cmake/KwaveRPMSupport.cmake
@@ -23,7 +23,7 @@ FIND_PROGRAM(RM_EXECUTABLE NAMES rm)
 FIND_PROGRAM(TAR_EXECUTABLE NAMES tar) # we need tar-1.16 or newer !
 FIND_PROGRAM(RPM_EXECUTABLE NAMES rpm)
 FIND_PROGRAM(RPMBUILD_EXECUTABLE NAMES rpmbuild)
-FIND_PROGRAM(GZIP_EXECUTABLE NAMES gzip)
+FIND_PROGRAM(BZIP2_EXECUTABLE NAMES bzip2)
 
 #############################################################################
 ### macro for extracting a field from the kwave.lsm file                  ###
@@ -83,10 +83,12 @@ CONFIGURE_FILE(
 ### "make tarball"                                                        ###
 
 SET(_tarball /tmp/kwave-${RPM_FULL_VERSION}.tar)
-SET(_tarball_gz ${_tarball}.gz)
+SET(_tarball_bz2 ${_tarball}.bz2)
 
-ADD_CUSTOM_COMMAND(OUTPUT ${_tarball_gz}
-    COMMAND ${TAR_EXECUTABLE} -c --exclude=.svn
+ADD_CUSTOM_COMMAND(OUTPUT ${_tarball_bz2}
+    COMMAND ${TAR_EXECUTABLE}
+        -c
+        --exclude=.svn
         --owner=root --group=root
         -C ${CMAKE_SOURCE_DIR}
         --transform=s+^./+kwave-${RPM_SHORT_VERSION}/+g
@@ -97,13 +99,13 @@ ADD_CUSTOM_COMMAND(OUTPUT ${_tarball_gz}
         -C ${CMAKE_BINARY_DIR}
         --transform=s+^./+kwave-${RPM_SHORT_VERSION}/+g
         ./${_specfile}
-    COMMAND ${RM_EXECUTABLE} -f ${_tarball_gz}
-    COMMAND ${GZIP_EXECUTABLE} ${_tarball}
+    COMMAND ${RM_EXECUTABLE} -f ${_tarball_bz2}
+    COMMAND ${BZIP2_EXECUTABLE} ${_tarball}
     DEPENDS ${CMAKE_BINARY_DIR}/${_specfile}
 )
 
 ADD_CUSTOM_TARGET(tarball
-    DEPENDS ${_tarball_gz}
+    DEPENDS ${_tarball_bz2}
 )
 
 #############################################################################
@@ -119,13 +121,13 @@ SET(_src_rpm ${_rpm_topdir}/SRPMS/kwave-${RPM_FULL_VERSION}.src.rpm)
 
 ADD_CUSTOM_COMMAND(OUTPUT ${_src_rpm}
     COMMAND ${MKDIR_EXECUTABLE} -p ${_rpm_topdir}/{SPECS,SOURCES,RPMS,SRPMS,BUILD}
-    COMMAND ${TAR_EXECUTABLE} -x -O -f ${_tarball_gz} --wildcards \\*.spec >
+    COMMAND ${TAR_EXECUTABLE} -x -O -f ${_tarball_bz2} --wildcards \\*.spec >
         ${_rpm_topdir}/SPECS/kwave-${RPM_FULL_VERSION}.spec
     COMMAND ${CMAKE_COMMAND} -E copy_if_different
-        ${_tarball_gz} ${_rpm_topdir}/SOURCES/kwave-${RPM_FULL_VERSION}.tar.gz
+        ${_tarball_bz2} ${_rpm_topdir}/SOURCES/kwave-${RPM_FULL_VERSION}.tar.bz2
     COMMAND ${RPMBUILD_EXECUTABLE} -bs --nodeps
         ${_rpm_topdir}/SPECS/kwave-${RPM_FULL_VERSION}.spec
-    DEPENDS ${_tarball_gz}
+    DEPENDS ${_tarball_bz2}
 )
 
 ADD_CUSTOM_TARGET(src_rpm

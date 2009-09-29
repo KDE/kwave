155a6159e6c0c0ca586ac191bfefb3959e73e018
Thomas Eschenbacher <Thomas.Eschenbacher@gmx.de>
2009-09-29 19:04:31 +0000

    L10N: do not build for en as default language, show message only
    svn-id: https://kwave.svn.sourceforge.net/svnroot/kwave/trunk@2459 bbf32e5a-961d-0410-9e10-91f32741c31e

--- cmake/KwaveL10N.cmake
+++ cmake/KwaveL10N.cmake
@@ -18,12 +18,13 @@
 #############################################################################
 ### get environment variable LINGUAS, default to all languages            ###
 
-SET(LINGUAS $ENV{LINGUAS})
+SET(LINGUAS "$ENV{LINGUAS}")
+STRING(REGEX REPLACE "[ \t]+" \; OUTPUT "${LINGUAS}")
 SEPARATE_ARGUMENTS(LINGUAS)
-IF ("x${LINGUAS}" STREQUAL "x")
+IF ("${LINGUAS}" STREQUAL "")
     SET(LINGUAS ${KWAVE_LINGUAS})
     MESSAGE(STATUS "LINGUAS not set, building for all supported languages")
-ENDIF ("x${LINGUAS}" STREQUAL "x")
+ENDIF ("${LINGUAS}" STREQUAL "")
 
 #############################################################################
 ### filter out only languages that are supported by Kwave                 ###
@@ -34,22 +35,18 @@ FOREACH (_lingua ${LINGUAS})
         STRING(REGEX MATCH "^[^:]+" _lang ${_known_lang})
         STRING(REGEX MATCH "[^:]+$$" _lang_name ${_known_lang})
         IF (${_lang} STREQUAL ${_lingua_short})
-            SET(KWAVE_BUILD_LINGUAS
-                ${KWAVE_BUILD_LINGUAS}
-                "${_lang}:${_lang_name};"
-            )
+            LIST(APPEND KWAVE_BUILD_LINGUAS "${_lang}:${_lang_name};")
             MESSAGE(STATUS "Enabled language support for ${_lingua_short} (${_lang_name})")
         ENDIF (${_lang} STREQUAL ${_lingua_short})
     ENDFOREACH(_known_lang)
 ENDFOREACH(_lingua)
 
 #############################################################################
-### default to english if no suitable language found                      ###
+### show a message if no suitable language found                          ###
 
-IF ("x${KWAVE_BUILD_LINGUAS}" STREQUAL "x")
-    MESSAGE(STATUS "Found no suitable language to build for, defaulting to english")
-    SET(KWAVE_BUILD_LINGUAS en:English)
-ENDIF ("x${KWAVE_BUILD_LINGUAS}" STREQUAL "x")
+IF ("${KWAVE_BUILD_LINGUAS}" STREQUAL "")
+    MESSAGE(STATUS "Found no suitable language to build for")
+ENDIF ("${KWAVE_BUILD_LINGUAS}" STREQUAL "")
 
 #############################################################################
 #############################################################################

02f64318efb26f0133d430f727d0285495d5c108
Thomas Eschenbacher <Thomas.Eschenbacher@gmx.de>
2010-01-08 19:19:29 +0000

    introduced 'application context' object
    svn-id: https://kwave.svn.sourceforge.net/svnroot/kwave/branches/signal_views@2501 bbf32e5a-961d-0410-9e10-91f32741c31e

--- kwave/KwaveApp.cpp
+++ kwave/KwaveApp.cpp
@@ -26,11 +26,14 @@
 #include <kconfiggroup.h>
 #include <ktoolinvocation.h>
 
+#include "libkwave/ApplicationContext.h"
 #include "libkwave/ClipBoard.h"
 #include "libkwave/LabelList.h"
 #include "libkwave/KwaveSampleArray.h"
 #include "libkwave/MemoryManager.h"
 #include "libkwave/Parser.h"
+#include "libkwave/SignalManager.h"
+#include "libkwave/PluginManager.h"
 
 #include "TopWidget.h"
 #include "KwaveApp.h"
@@ -134,16 +137,15 @@ void KwaveApp::addRecentFile(const QString &newfile)
 bool KwaveApp::newWindow(const KUrl &url)
 {
     KwaveSplash::showMessage(i18n("Opening main window..."));
-    TopWidget *new_top_widget = new TopWidget(*this);
-    Q_ASSERT(new_top_widget);
-    if (!new_top_widget) return false;
 
-    if ( !(new_top_widget->isOK()) ) {
-	qWarning("KwaveApp::newWindow() failed!");
-	delete new_top_widget;
+    Kwave::ApplicationContext *context = new Kwave::ApplicationContext(*this);
+    if (!context) return false;
+    if (!context->init()) {
+	delete context;
 	return false;
     }
 
+    TopWidget *new_top_widget = context->topWidget();
     if (m_topwidget_list.isEmpty()) {
 	// the first widget is the main widget !
 	setTopWidget(new_top_widget); // sets geometry and other properties

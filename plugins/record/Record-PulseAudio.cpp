523438ed93d5f445eb95584f2504407a3e2d0a24
Joerg-Christian Boehme <joerg@chaosdorf.de>
2014-03-09 23:19:56 +0100

    Add author and license header and change docu.

--- plugins/record/Record-PulseAudio.cpp
+++ plugins/record/Record-PulseAudio.cpp
@@ -1,3 +1,20 @@
+/***************************************************************************
+ *    Record-PulseAudio.h  -  device for audio recording via PulesAudio
+ *                             -------------------
+ *    begin                : Sun Okt 20 2013
+ *    copyright            : (C) 2005 by Thomas Eschenbacher
+ *    email                : Thomas.Eschenbacher@gmx.de
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #include "config.h"
 #ifdef HAVE_PULSEAUDIO_SUPPORT
 

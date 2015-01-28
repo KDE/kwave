f480fec43acb2c8aceb0009164c8e224ce2f398f
Albert Astals Cid <aacid@kde.org>
2015-01-28 20:34:30 +0100

    i18n fixes
    Remove unneeded flags from XGETTEXT call
    Tell scripty that the .desktop.in file is a .desktop file

--- Messages.sh
+++ Messages.sh
@@ -19,15 +19,8 @@
 # collect all messages from the .ui files
 $EXTRACTRC `find . -name \*.ui | sort` > _kwave-i18n-ui.cpp
 
-# get the strings from the .desktop file
-intltool-extract --quiet --type=gettext/ini kwave/kwave.desktop.in
-
 # fetch all messages from the C++ source and header files
-$XGETTEXT --from-code=UTF-8 -C --kde \
-        -ci18n -ki18n:1 -ki18nc:1c,2 -ki18np:1,2 -ki18ncp:1c,2,3 -ktr2i18n:1 \
-        -kI18N_NOOP:1 -kI18N_NOOP2:1c,2 -kaliasLocale -kki18n:1 -kki18nc:1c,2 \
-        -kki18np:1,2 -kki18ncp:1c,2,3 -kN_:1 \
-        `find . -name \*.cpp -o -name \*.h | sort` \
+$XGETTEXT `find . -name \*.cpp -o -name \*.h | sort` \
         -o _kwave-i18n-src.pot
 
 # create a pot file from menus.config
@@ -37,6 +30,6 @@ perl bin/menusconfig2pot.pl kwave/menus.config _kwave-i18n-menu.pot
 msgcat _kwave-i18n-src.pot _kwave-i18n-menu.pot -o $podir/kwave.pot
 
 # clean up
-rm -f _kwave-i18n* kwave/kwave.desktop.in.h
+rm -f _kwave-i18n*
 
 ### EOF ###

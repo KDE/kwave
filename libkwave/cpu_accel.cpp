417f729a837d2d18f0938d02e696a8808fe833ae
Thomas Eschenbacher <Thomas.Eschenbacher@gmx.de>
2015-11-11 06:16:13 +0100

    fixed libkwave/cpu_accel.cpp, removed duplicate entry from CMakeLists.txt

--- libkwave/cpu_accel.cpp
+++ libkwave/cpu_accel.cpp
@@ -394,3 +394,36 @@ static uint32_t arch_accel (void)
 }
 #endif
 #endif /* ARCH_SPARC */
+
+uint32_t xine_mm_accel (void)
+{
+  static int initialized = 0;
+  static uint32_t accel = 0;
+
+  if (!initialized) {
+#ifdef HAVE_MLIB
+#ifdef MLIB_LAZYLOAD
+    void *hndl;
+
+    if ((hndl = dlopen("libmlib.so.2", RTLD_LAZY | RTLD_GLOBAL | RTLD_NODELETE)) != NULL) {
+      dlclose(hndl);
+      accel |= MM_ACCEL_MLIB;
+    }
+#else
+    accel |= MM_ACCEL_MLIB;
+#endif
+#endif
+
+#if defined(__i386__) || defined(__x86_64__) || (defined(ARCH_PPC) && defined(ENABLE_ALTIVEC)) || (defined(ARCH_SPARC) && defined(ENABLE_VIS))
+    accel |= arch_accel();
+#endif
+
+    if(getenv("XINE_NO_ACCEL")) {
+      accel = 0;
+    }
+
+    initialized = 1;
+  }
+
+  return accel;
+}

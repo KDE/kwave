c20b032ba2a3e493c8a0070ff82f8ca2f555f59e
Thomas Eschenbacher <Thomas.Eschenbacher@gmx.de>
2017-01-13 21:07:23 +0100

    - changed licenses of all cmake files to BSD license - removed cmake/FindOggVorbis.cmake

--- cmake/KwavePulseAudioSupport.cmake
+++ cmake/KwavePulseAudioSupport.cmake
@@ -7,33 +7,34 @@
 #############################################################################
 #
 #############################################################################
-##                                                                          #
-##    This program is free software; you can redistribute it and/or modify  #
-##    it under the terms of the GNU General Public License as published by  #
-##    the Free Software Foundation; either version 2 of the License, or     #
-##    (at your option) any later version.                                   #
-##                                                                          #
+#                                                                           #
+# Redistribution and use in source and binary forms, with or without        #
+# modification, are permitted provided that the following conditions        #
+# are met:                                                                  #
+#                                                                           #
+# 1. Redistributions of source code must retain the above copyright         #
+#    notice, this list of conditions and the following disclaimer.          #
+# 2. Redistributions in binary form must reproduce the above copyright      #
+#    notice, this list of conditions and the following disclaimer in the    #
+#    documentation and/or other materials provided with the distribution.   #
+#                                                                           #
+# For details see the accompanying cmake/COPYING-CMAKE-SCRIPTS file.        #
+#                                                                           #
 #############################################################################
 
 OPTION(WITH_PULSEAUDIO "enable playback/recording via PulseAudio [default=on]" ON)
 
 IF (WITH_PULSEAUDIO)
 
-INCLUDE(FindPkgConfig)
-INCLUDE(UsePkgConfig)
+    INCLUDE(FindPkgConfig)
+    INCLUDE(UsePkgConfig)
 
-PKG_CHECK_MODULES(PULSEAUDIO REQUIRED libpulse>=0.9.16)
-IF (NOT PULSEAUDIO_FOUND)
-    MESSAGE(FATAL_ERROR "PulseAudio library not found")
-ENDIF(NOT PULSEAUDIO_FOUND)
+    PKG_CHECK_MODULES(PULSEAUDIO REQUIRED libpulse>=0.9.16)
 
-PKG_CHECK_MODULES(PULSEAUDIO libpulse)
+    MESSAGE(STATUS "Found PulseAudio library in ${PULSEAUDIO_LIBDIR}")
+    MESSAGE(STATUS "Found PulseAudio headers in ${PULSEAUDIO_INCLUDEDIR}")
 
-# MESSAGE(STATUS "Found PulseAudio version ${PULSEAUDIO_VERSION}")
-MESSAGE(STATUS "Found PulseAudio library in ${PULSEAUDIO_LIBDIR}")
-MESSAGE(STATUS "Found PulseAudio headers in ${PULSEAUDIO_INCLUDEDIR}")
-
-SET(HAVE_PULSEAUDIO_SUPPORT  ON CACHE BOOL "enable PulseAudio support")
+    SET(HAVE_PULSEAUDIO_SUPPORT  ON CACHE BOOL "enable PulseAudio support")
 
 ENDIF (WITH_PULSEAUDIO)
 

355379256ecc88d8a588d69a704ac279ff50a249
Thomas Eschenbacher <Thomas.Eschenbacher@gmx.de>
2026-09-19 10:28:53 +0200

    rate converter: made more robust against invalid ratio settings

--- libkwave/modules/RateConverter.cpp
+++ libkwave/modules/RateConverter.cpp
@@ -50,7 +50,8 @@ void Kwave::RateConverter::goOn()
 void Kwave::RateConverter::input(Kwave::SampleArray &data)
 {
     // shortcut for ratio == 1:1
-    if ((m_ratio == 1.0) || data.isEmpty()) {
+    if (qFuzzyCompare(m_ratio, 0.0) || qFuzzyCompare(m_ratio, 1.0) ||
+        data.isEmpty()) {
         output(data);
         return;
     }
